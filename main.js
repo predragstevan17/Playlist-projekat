0d0ac357424d6a5d68a18c8505385b7937dab896
predragstevan17 <44097540+predragstevan17@users.noreply.github.com>
2019-04-23 22:22:43 +0200

    Filter and sort functions added

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const songlist = document.getElementById('song-list');
+const songList = document.getElementById('song-list');
 
 let songs = [
-    { artist: 'Led Zeppelin', song: 'Good Times, Bad Times', score: 97,id: 1 },
-    { artist: 'Deep Purple', song: 'Child in Time', score: 100,id: 2 },
-    { artist: 'Eric Johnson', song: 'Cliffs of Dover', score: 95,id: 3 },
+    { artist: 'Led Zeppelin', song: 'Good Times, Bad Times', score: 97, id: 1 },
+    { artist: 'Deep Purple', song: 'Child in Time', score: 100, id: 2 },
+    { artist: 'Eric Johnson', song: 'Cliffs of Dover', score: 95, id: 3 },
 
 ]
 
 function displaySongs(songs) {
-    
-    songlist.innerHTML = '';
+
+    songList.innerHTML = '';
     songs.forEach(function (song) {
-        songlist.innerHTML += `
+        songList.innerHTML += `
         <tr>
             <td><i class="fab fa-itunes-note"></i></td>
             <td>${song.artist}</td>
@@ -45,17 +45,53 @@ function addSong() {
     songs.push(newSong);
 }
 
-songlist.addEventListener('click',function(event){
-    if(event.target.classList.contains('remove')){
+songList.addEventListener('click', function (event) {
+    if (event.target.classList.contains('remove')) {
         removeSong(event.target);
     }
 });
 
-function removeSong(target){
+function removeSong(target) {
     const id = Number(target.getAttribute('data-id'));
-    songs = songs.filter(function(song){
+    songs = songs.filter(function (song) {
         return song.id !== id;
     })
     console.log(songs);
     target.parentElement.parentElement.remove();
 }
+
+const search = document.getElementById('search');
+
+search.addEventListener('input', filterPlaylist);
+
+function filterPlaylist(event){
+   let query = event.target.value.toLowerCase();
+   let trList = songList.querySelectorAll('TR');
+   for(let i = 0; i < trList.length; i++){
+       let tds = trList[i].querySelectorAll('TD');
+       let artistTd = tds[1];
+       let titleTd = tds[2];
+       if(tds[1].innerText.toLowerCase().indexOf(query) !== -1 || tds[2].innerText.toLowerCase().indexOf(query) !== -1){
+           //ostavi pesmu na spisku
+            titleTd.parentElement.style.display = ''
+       }
+       else{
+           console.log('ne');
+           //skloni pesmu
+           titleTd.parentElement.style.display = 'none'
+       }
+   }
+}
+
+function sortByScore(songs){
+    //sortiraj niz
+    songs.sort(function(a,b){
+        return b.score - a.score;
+    })
+    console.log(songs);
+
+    //prikazi sortirani niz
+    displaySongs(songs);
+}
+
+sortByScore(songs);
